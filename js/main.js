e5f5f50690b959745576cb1f6a28b6f771699ab8
Евгений <chebyrechka@mail.ru>
2022-02-26 20:47:07 +0300

    нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const getRandomIntNumber = function (from, to) {
-  if (to <= from) {
-    throw new Error('Значение from должно быть больше , чем to');
+  if (from < to) {
+    throw new Error('Значение from должно быть меньше, to');
   }
-  return Math.random() * (to - from + 1) + from;
+  return Math.floor(Math.random() * (to - from + 1) + from);
 };
-getRandomIntNumber(1, 100);
 
-function checkMaxLength (string, length) {
-  return string.length <= length;
+console.log(getRandomIntNumber(1, 100));
+
+function checkMaxLength (string , maxLength) {
+
+  return string.length <= maxLength;
 }
-checkMaxLength(1, 100);
+
+console.log(checkMaxLength('кекс',4));
