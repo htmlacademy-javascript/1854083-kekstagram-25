e733b6be48a812a7af9445a2f14ddf56015fadc2
Евгений <chebyrechka@mail.ru>
2022-03-06 20:38:05 +0300

    Больше деталей 1.3

--- js/main.js
+++ js/main.js
@@ -63,60 +63,45 @@ const COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const USERS_PHOTOS = 25;
-
-const MIN_NUMBER_ID_DESCRIPTION = 1;
-
-const MAX_NUMBER_ID_DESCRIPTION = 25;
+const PHOTOS_COUNT = 25;
 
 const RANDOM_AVATAR_FROM = 1;
 
 const RANDOM_AVATAR_TO = 6;
 
-const ADDRESS_IMAGE_FROM = 1;
-
-const ADDRESS_IMAGE_TO = 25;
-
 const MIN_QUANTITY_LIKES = 15;
 
 const MAX_QUANTITY_LIKES = 200;
 
+const COMMENTS_COUNT = 20;
+
 const getRandomIntNumber = function (a, b) {
-  if (a < b) {
+  if (a > b) {
     throw new Error('Значение a должно быть меньше, b');
   }
   return Math.floor(Math.random() * (b - a + 1) + a);
 };
 
-function checkMaxLength (string , maxLength) {
-  return string.length <= maxLength;
-}
+const checkMaxLength = (string, maxLength) => string.length <= maxLength;
 
-const getRandomElement = (elements) => {
-  return elements[getRandomElement(0, elements.length - 1)];
-};
+const getRandomElement = (elements) => elements[getRandomIntNumber(0, elements.length - 1)];
 
-const generateComments = () => {
-  return {
-  id: getRandomIntNumber(MIN_NUMBER_ID_DESCRIPTION, MAX_NUMBER_ID_DESCRIPTION),
-  avatar: `img/avatar(${getRandomIntNumber(RANDOM_AVATAR_FROM, RANDOM_AVATAR_TO)}).svg`,
+
+const generateComment = (id) => ({
+  id,
+  avatar: `img/avatar-${getRandomIntNumber(RANDOM_AVATAR_FROM, RANDOM_AVATAR_TO)}.svg`,
   message: getRandomElement(COMMENTS),
   name: getRandomElement(NAMES),
-};
-}
- 
-const createDescriptionPhoto = (id, url) => {
-  return {
-    id: getRandomIntNumber(MIN_NUMBER_ID_DESCRIPTION, MAX_NUMBER_ID_DESCRIPTION),
-    url: `photos/(${ getRandomIntNumber(ADDRESS_IMAGE_FROM, ADDRESS_IMAGE_TO)}).jpg`,
-    description: getRandomElement(DESCRIPTIONS),
-    likes: getRandomIntNumber(MIN_QUANTITY_LIKES, MAX_QUANTITY_LIKES),
-    comment: 
-  };
-};
+});
 
+const createDescriptionPhoto = (id) => ({
+  id,
+  url: `photos/${id}.jpg`,
+  description: getRandomElement(DESCRIPTIONS),
+  likes: getRandomIntNumber(MIN_QUANTITY_LIKES, MAX_QUANTITY_LIKES),
+  comments: Array.from({length: COMMENTS_COUNT}, (_, index) => generateComment(index)),
+});
 
-
-const photos = Array.from({length: USERS_PHOTOS}, (_, index) => createDescriptionPhoto(index));
+const photos = Array.from({length: PHOTOS_COUNT}, (_, index) => createDescriptionPhoto(index));
 
 console.log(photos);
